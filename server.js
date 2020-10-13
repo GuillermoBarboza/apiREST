15cb1bdad88725a4abbf82738f96623d894ccec4
Martin Magnone <tinchomagnone23@gmail.com>
2020-10-13 14:37:23 -0300

    model user

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const routes = require('./routes');
+const mongoose = require("mongoose");
 const app = express();
+
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
 
 app.set('view engine', 'ejs');
 
@@ -20,6 +24,12 @@ app.use(
 )
 
 app.use(routes)
+
+mongoose.connect(`mongodb://localhost/${process.env.DATABASE_NAME}`);
+mongoose.connection
+ .once("open", () => console.log("¡Conexión con la base de datos establecida!"))
+ .on("error", error => console.log(error));
+
 
 app.listen(3000, () => {
     console.log("listening on 3000");
