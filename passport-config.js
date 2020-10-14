612072978d08f136a9f6d4702b82d846575fee19
Martin Magnone <tinchomagnone23@gmail.com>
2020-10-14 14:22:04 -0300

    login username and email

--- passport-config.js
+++ passport-config.js
@@ -21,7 +21,7 @@ module.exports = {
   localStrategy: function () {
   passport.use(new LocalStrategy(
     function(username, password, done) {
-      User.findOne({ username: username }, function (err, user) {
+      User.findOne({$or:[{ username: username}, {email: username}] }, function (err, user) {
         
         if (err) { return done(err); }
         if (!user) { return done(null, false, { message: "Datos Incorrectos."}); }
