d76115dc68be5d79d5b19d2b9e55d7960ff1f02c
Guishe <gui.bar.jim@gmail.com>
2020-10-19 18:55:13 -0300

    push del vercel

--- passport-config.js
+++ passport-config.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require('bcryptjs')
-const User = require('../twitter_clone/models/User');
+const User = require('./models/User');
 
 
 
