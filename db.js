6d21fc5b1ce8ccf9d6c2f1fa2906024928464d03
Guishe <gui.bar.jim@gmail.com>
2020-10-19 17:31:39 -0300

    mongoose connection

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 
-mongoose.connect(`mongodb://localhost/${process.env.DATABASE_NAME}`);
+mongoose.connect(`mongodb+srv://Guishe:5e*q2$J8c9h3.6W@cluster0.uxix3.gcp.mongodb.net/el_twitter?retryWrites=true&w=majority`);
 
 
 
