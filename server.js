9d4337fa998b347718e6bdbc13fdabfd921c5913
Guishe <gui.bar.jim@gmail.com>
2020-11-03 12:03:54 -0300

    boilerplate con vercel

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.set("views", path.join(__dirname, "views")); 
-
-app.set('view engine', 'ejs');
 
 app.use(
     session({
