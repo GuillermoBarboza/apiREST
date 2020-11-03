1ee84d2d5dc1a1321aacdf11c37989666bb22d15
Martin Magnone <tinchomagnone23@gmail.com>
2020-11-03 18:09:58 -0300

    back

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const session = require('express-session');
-const routes = require('./routes')
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const routes = require("./routes");
 const passport = require("passport");
 const initializePassport = require("./passport-config");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 // PATH.JOIN porque sino el build de vercel(deploy) no entiende
-const path = require('path')
+const path = require("path");
 
-app.use(cors())
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
 app.use(
-    session({
-        secret: 'twita',
-        resave: true,
-        saveUninitialized: true,// resave y save uninitialized
-    }) 
-)
+  session({
+    secret: "twita",
+    resave: true,
+    saveUninitialized: true, // resave y save uninitialized
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 initializePassport.localStrategy(passport);
 
+app.use(routes);
 
-app.use(routes)
-
-app.listen(3000, () => {
-  console.log("listening on 3000");
+app.listen(8000, () => {
+  console.log("listening on 8000");
 });
