a3065fefdb07f2e8dae37752fcc2d19e820b4065
Martin Magnone <tinchomagnone23@gmail.com>
2020-11-06 14:31:55 -0300

    rutas corregidas

--- routes.js
+++ routes.js
@@ -1,55 +1,76 @@
 const express = require("express");
 const router = express();
-const controllers = require('./controllers/controllers')
-const path = require('path')
-const jwtMiddleware = require('express-jwt')
+const controllers = require("./controllers/controllers");
+const path = require("path");
+const jwtMiddleware = require("express-jwt");
 
 //RUTAS
 router.get("/css/styles.css", (req, res) => {
-    res.sendFile(__dirname + "/public/css/styles.css");
+  res.sendFile(__dirname + "/public/css/styles.css");
 });
 router.get("js/app.js", (req, res) => {
-    res.sendFile(__dirname + "/public/js/app.js");
+  res.sendFile(__dirname + "/public/js/app.js");
 });
 
- /* router.get('/', controllers.fillDb)  */
+/* router.get('/', controllers.fillDb)  */
 
 /* RUTAS API */
-router.get('/api/twits/feed', jwtMiddleware({secret: process.env.JWTKEY, algorithms: ["HS256"]}), controllers.homeFeed)
+//RUTAS / TWITS
+router.get(
+  "/api/twits/feed",
+  jwtMiddleware({ secret: process.env.JWTKEY, algorithms: ["HS256"] }),
+  controllers.homeFeed
+);
 
-router.post('/api/twits', jwtMiddleware({secret: process.env.JWTKEY, algorithms: ["HS256"]}), controllers.createTwit)
+router.post(
+  "/api/twits",
+  jwtMiddleware({ secret: process.env.JWTKEY, algorithms: ["HS256"] }),
+  controllers.createTwit
+);
 
-router.get('/api/profile')
-
-
-
-// USER PROFILE
-router.get('/profile/:username', controllers.getUserProfile)
-// discover users route
-router.get('/discover', controllers.discoverFeed)
-// Twitealo ruta
-router.post('/twitealo', controllers.isLoggedIn, controllers.createTwit)
-
-// Follow someone 
-router.post('/follow', controllers.isLoggedIn, controllers.followUnfollow)
-// LIKES
-router.post('/like',controllers.isLoggedIn, controllers.like)
-// Login routes
-router.get('/login', controllers.logInView)
-
-router.post('/login', controllers.signIn)
-
-router.get("/logout", controllers.logout);
+// Twitealo ruta creat twit
+router.post(
+  "/api/twits/twitealo",
+  controllers.isLoggedIn,
+  controllers.createTwit
+);
 
 //delete twit
-router.get("/eliminar/:id", controllers.isLoggedIn, controllers.deleteTwit)
+router.get(
+  "/api/twits/eliminar/:id",
+  controllers.isLoggedIn,
+  controllers.deleteTwit
+);
 
-router.get('/settings', controllers.userSettings)
+// LIKES
+router.post("api/twits/like", controllers.isLoggedIn, controllers.like);
+
+router.get("/api/profile");
+//
+
+// RUTAS /USER
+
+//LOGIN
+router.post("/api/users/login", controllers.signIn);
+router.get("/login", controllers.logInView);
+//LOGOUT
+router.get("/logout", controllers.logout);
+//REGISTRO
+router.post("/api/users/registro", controllers.signUp);
+router.get("/registro", controllers.registerView);
+// discover users route
+router.get("/api/users/discover", controllers.discoverFeed);
+// USER PROFILE
+router.get("api/users/profile/:username", controllers.getUserProfile);
+// settings
+router.get("api/users/settings", controllers.userSettings);
+// Follow someone
+router.post(
+  "api/users/follow",
+  controllers.isLoggedIn,
+  controllers.followUnfollow
+);
 
 // sign up routes
-router.get('/registro', controllers.registerView)
-router.post("/registro", controllers.signUp)
-
-
 
 module.exports = router;
